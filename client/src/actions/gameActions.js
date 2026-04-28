4cca05620871962a17e251c8d125726f1aad74dc
Chau Nguyen on Windows <nguyench4u@gmail.com>
2026-04-28 00:44:09 -0600

    Redux Fixes and Package Imports

---
+++ client/src/actions/gameActions.js
@@ -0,0 +1,62 @@
+// gameActions.js
+// Actions are functions that return an object that contains a type and an optional payload
+// These objects are dispatched to the Redux store to trigger state changes
+// AKA the actual API calls
+
+import { FETCH_GAMES } from '../constants/actionTypes';
+
+const API_URL = process.env.REACT_APP_API_URL; // Get the API URL from environment variables
+
+// fetchGames
+export const fetchGames = () => async (dispatch) => {
+    try {
+        const response = await fetch(`${API_URL}/api/games`); // Get fetch data
+        const data = await response.json(); // Parse the JSON response
+        dispatch({ type: FETCH_GAMES, payload: data.games }); // Dispatch the action with the games data
+    } catch (err) {
+        console.error('Error fetching games: ', err);
+    }
+}; 
+
+
+// addGame
+export const addGame = (gameData) => async (dispatch) => {
+    try {
+        await fetch(`${API_URL}/api/games`, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(gameData),
+        });
+        dispatch(fetchGames()); // Refresh the games list after adding a new game
+    } catch (err) {
+        console.error('Error adding game: ', err)
+    }
+};
+
+
+// updateGame
+export const updateGame = (id, gameData) => async (dispatch) => {
+    try {
+        await fetch(`${API_URL}/api/games/${id}`, {
+            method: 'PUT',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(gameData),
+        });
+        dispatch(fetchGames()); // Refresh the games list after updating a game
+    } catch (err) {
+        console.error('Error updating game: ', err);
+    }
+};
+
+
+// deleteGame
+export const deleteGame = (id) => async (dispatch) => {
+    try {
+        await fetch (`${API_URL}/api/games/${id}`, {
+            method: 'DELETE',
+        });
+        dispatch(fetchGames()); // Refresh the games list after deleting a game
+    } catch (err) {
+        console.error('Error deleting game: ', err);
+    }
+};
--- client/src/stores/store.js
+++ client/src/stores/store.js
@@ -2,7 +2,7 @@
 // Store is the central place where the state of the application is stored in Redux
 
 import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
+import { thunk } from 'redux-thunk';
 import gameReducer from '../reducers/gameReducer';
 
 // Create the Redux store with the gameReducer and apply thunk middleware for async actions
